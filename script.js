984976c12641bf3cebd45a94f3508c9a3b19e194
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-16 17:38:30 -0300

    update script.js

--- script.js
+++ script.js
@@ -2,12 +2,5 @@ function iniciar(){
 	document.getElementById("palavra").innerHTML = `${worlds[0]}`;
 }
 
-function confirmar(){
-	
-}
-
-function resposta(){
-	
-}
 
 let worlds = ['um','dois','três'];
