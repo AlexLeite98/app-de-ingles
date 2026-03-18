4fdb05ddef5d58d0f6cb211d6e2801dc3629da4c
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-18 13:07:14 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Aqui fica as funções que serão reutilizadas com frequencia.
 const d20 = Math.floor(Math.random()*(20-1 + 1)+1);
-const d20 = Math.floor(Math.random()*(12-1 + 1)+1);
-const d20 = Math.floor(Math.random()*(8-1 + 1)+1);
-const d20 = Math.floor(Math.random()*(6-1 + 1)+1);
-const d20 = Math.floor(Math.random()*(4-1 + 1)+1);
+const d12 = Math.floor(Math.random()*(12-1 + 1)+1);
+const d8 = Math.floor(Math.random()*(8-1 + 1)+1);
+const d6 = Math.floor(Math.random()*(6-1 + 1)+1);
+const d4 = Math.floor(Math.random()*(4-1 + 1)+1);
 
 // Aqui ficará os dados dos personagens.
 const personagem1 = {
